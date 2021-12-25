de290dc34b5dcef5beeee1928175c33c2c9e3098
Jalmijao <jalmiriago@usp.br>
2021-12-25 15:41:48 -0300

    criacao do router e base das telas

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Dashboard from "./pages/dashboard/Dashboard";
+import Produtos from "./pages/produtos/Produtos";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Servicos from "./pages/servicos/Servicos";
+import Funcionarios from "./pages/funcionarios/Funcionarios";
+import ProgramaFidelidade from "./pages/programaFidelidade/ProgramaFidelidade";
+import Fornecedores from "./pages/fornecedores/Fornecedores";
+import SolicitarProduto from "./pages/solicitarProduto/SolicitarProduto";
+import Vender from "./pages/vender/Vender";
+
 function App() {
   return (
-    <div>
+    <Router>
      <Topbar/>
      <div className="container">
       <Sidebar/>
-      <Dashboard/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>} />
+        <Route path='/produtos' element={<Produtos/>} />
+        <Route path='/servicos' element={<Servicos/>} />
+        <Route path='/funcionarios' element={<Funcionarios/>} />
+        <Route path='/fidelidade' element={<ProgramaFidelidade/>} />
+        <Route path='/fornecedores' element={<Fornecedores/>} />
+        <Route path='/solicitarProduto' element={<SolicitarProduto/>} />
+        <Route path='/vender' element={<Vender/>} />
+
+      </Routes>
      </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,35 +1,59 @@
 import "./Sidebar.css";
-import {Dashboard,Person,Medication} from '@mui/icons-material/';
+import {Dashboard,Person,Medication,MedicalServices,Loyalty,LocalShipping,LocalGroceryStore} from '@mui/icons-material/';
 import userIcon from '../../images/userIcon.png'; 
-
+import {
+    Link,
+  } from "react-router-dom";
 export default function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebarWrapper">
                 <div className="sidebarMenu">
-                    <h3 className="sidebarTitle">Main</h3>
+                    <h3 className="sidebarTitle">Vendas</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem active">
-                            <Dashboard className="sidebarIcon"/> Dashboard
-                        </li>
-                        <li className="sidebarListItem">
-                            <Person className="sidebarIcon"/> Pessoa
-                        </li>
-                        <li className="sidebarListItem">
-                            <Medication className="sidebarIcon"/> Produtos
-                        </li>
+                        <Link to="/">
+                            <li className="sidebarListItem">
+                                <Dashboard className="sidebarIcon"/> Dashboard
+                            </li>
+                        </Link>
+                        <Link to="/servicos">
+                            <li className="sidebarListItem">
+                                <MedicalServices className="sidebarIcon"/> Serviços
+                            </li>
+                        </Link>
+                        <Link to="/vender">
+                            <li className="sidebarListItem">
+                                <LocalGroceryStore className="sidebarIcon"/> Vender
+                            </li>
+                        </Link>
                     </ul>
-                    <h3 className="sidebarTitle">Components</h3>
+                    <h3 className="sidebarTitle">Gerenciar</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem">
-                            <Dashboard className="sidebarIcon"/> Dashboard
-                        </li>
-                        <li className="sidebarListItem">
-                            <Person className="sidebarIcon"/> Pessoa
-                        </li>
-                        <li className="sidebarListItem">
-                            <Medication className="sidebarIcon"/> Produtos
-                        </li>
+                        <Link to="/funcionarios">
+                            <li className="sidebarListItem">
+                                <Person className="sidebarIcon"/> Funcionários
+                            </li>
+                        </Link>
+                        <Link to="/produtos">
+                            <li className="sidebarListItem">
+                                <Medication className="sidebarIcon"/> Produtos
+                            </li>
+                        </Link>
+                        <Link to="/fidelidade">
+                            <li className="sidebarListItem">
+                                <Loyalty className="sidebarIcon"/> Programa de fidelidade
+                            </li>
+                        </Link>
+                        <Link to="/fornecedores">
+                            <li className="sidebarListItem">
+                                <LocalShipping className="sidebarIcon"/> Fornecedores
+                            </li>
+                        </Link>
+                        <Link to="/solicitarProduto">
+                            <li className="sidebarListItem">
+                                <Medication className="sidebarIcon"/> Solicitar Produtos ao Fornecedor
+                            </li>
+                        </Link>
                     </ul>
                 </div>
 
